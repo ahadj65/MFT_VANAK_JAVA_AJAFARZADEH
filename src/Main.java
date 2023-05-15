e3d374d8363ada08b1c86e1dee52052d0f64fcc0
209-Teacher <ahad.j.65@gmail.com>
2023-05-15 19:14:43 +0330

    operations

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
+        // Assignment, Arithmetic, Comparison, Logical
 
+        // Assignment Operator
+        int a = 45;
+
+        // Arithmetics operators +, -, *, /, %, ++, --
+
+        int myInt = 2;
+
+//        System.out.println(myInt++);
+//        System.out.println(++myInt);
+
+        // Comparison <, <=, >, >=, ==, !=
+        int i = 3, j =4, k = 6, w = 7;
+        boolean myCompare = i < i;
+
+        int myInt2 = 4;
+
+        myInt2 += 2; // myInt2 = myInt2 + 2;
+
+        Integer integer = 3;
+        int z = 4;
+
+
+        Math.max(i, j);
+
+
+//        System.out.println(i < i);
+
+        // logical operators &&, ||, !
+
+
+        System.out.println((int) (Math.random() * 100 + 10));
     }
 }
